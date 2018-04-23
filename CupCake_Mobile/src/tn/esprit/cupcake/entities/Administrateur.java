224296be5119a6eefc3e6695d4abe3a6169b4ce4
Alaa NFISSI <alaa.nfissi@esprit.tn>
2018-04-23 20:27:13 +0200

    getters & setters

--- CupCake_Mobile/src/tn/esprit/cupcake/entities/Administrateur.java
+++ CupCake_Mobile/src/tn/esprit/cupcake/entities/Administrateur.java
@@ -14,9 +14,5 @@ public class Administrateur extends Utilisateur{
     public Administrateur() {
     }
 
-    public Administrateur(long id_utilisateur, String password, String email, String pseudo, int etat_compte, int num_tel, String nom, String prenom, java.sql.Date date_naissance, int role) {
-        super(id_utilisateur, password, email, pseudo, etat_compte, num_tel, nom, prenom, date_naissance, role);
-    }
-
     
 }
--- CupCake_Mobile/src/tn/esprit/cupcake/entities/Utilisateur.java
+++ CupCake_Mobile/src/tn/esprit/cupcake/entities/Utilisateur.java
@@ -39,5 +39,138 @@ public class Utilisateur {
     public Utilisateur() {
     }
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUsername_canonical() {
+		return username_canonical;
+	}
+
+	public void setUsername_canonical(String username_canonical) {
+		this.username_canonical = username_canonical;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail_canonical() {
+		return email_canonical;
+	}
+
+	public void setEmail_canonical(String email_canonical) {
+		this.email_canonical = email_canonical;
+	}
+
+	public String getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getLast_login() {
+		return last_login;
+	}
+
+	public void setLast_login(Date last_login) {
+		this.last_login = last_login;
+	}
+
+	public String getConfirmation_token() {
+		return confirmation_token;
+	}
+
+	public void setConfirmation_token(String confirmation_token) {
+		this.confirmation_token = confirmation_token;
+	}
+
+	public Date getPassword_requested_at() {
+		return password_requested_at;
+	}
+
+	public void setPassword_requested_at(Date password_requested_at) {
+		this.password_requested_at = password_requested_at;
+	}
+
+	public String getRoles() {
+		return roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+
+	public int getNum_tel() {
+		return num_tel;
+	}
+
+	public void setNum_tel(int num_tel) {
+		this.num_tel = num_tel;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Date getDate_naissance() {
+		return date_naissance;
+	}
+
+	public void setDate_naissance(Date date_naissance) {
+		this.date_naissance = date_naissance;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur{" + "id=" + id + ", username=" + username + ", username_canonical=" + username_canonical + ", email=" + email + ", email_canonical=" + email_canonical + ", enabled=" + enabled + ", salt=" + salt + ", password=" + password + ", last_login=" + last_login + ", confirmation_token=" + confirmation_token + ", password_requested_at=" + password_requested_at + ", roles=" + roles + ", num_tel=" + num_tel + ", nom=" + nom + ", prenom=" + prenom + ", date_naissance=" + date_naissance + '}';
+	}
+
   
 }
